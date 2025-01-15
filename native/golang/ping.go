5d0358d50e8eb1709085e3a14295035ed7be5c54
Dan Kelosky <dkelosky@gmail.com>
2025-01-15 10:41:53 -0500

    add test to send binary data
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- native/golang/ping.go
+++ native/golang/ping.go
@@ -13,7 +13,6 @@ func main() {
   text, _ := reader.ReadString('\n')
   fmt.Printf("Input was '%s'", text)
 
-  // encodedStr := "MTIzNDU="
   decodedBytes, err := base64.StdEncoding.DecodeString(text)
   if err != nil {
     fmt.Println("Error:", err)
@@ -21,19 +20,6 @@ func main() {
   }
   fmt.Printf("Raw '%x'\n", decodedBytes)
   fmt.Println("String '%s'", string(decodedBytes))
-  // reader := bufio.NewReader(os.Stdin)
-
-  // for {
-  //   fmt.Print("Enter text (type 'quit' to exit): ")
-  //   input, _ := reader.ReadString('\n')
-  //   input = strings.TrimSpace(input)
-
-  //   if input == "quit" {
-  //     break
-  //   }
-
-  //   fmt.Println("You entered:", input)
-  // }
 
   fmt.Println("...ended!")
 
