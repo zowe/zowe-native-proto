493b0470bd3903031711637e72d542d453c2d5c8
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2025-01-27 13:47:59 -0500

    feat: get basic list of spool files
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- doc/apis.md
+++ doc/apis.md
@@ -4,14 +4,14 @@
 | List members     | ✅      | ✅         | ✅  | ✅  | ✅   |
 | List USS files   | ✅      | ✅         | ✅  | ✅  | ✅   |
 | List jobs        | ✅      | ✅         | ✅  | ✅  | ✅   |
-| List spool files | ✅      | ✅         | ✅  | ❌  | ✅   |
+| List spool files | ✅      | ✅         | ✅  | ✅  | ✅   |
 | Read data set    | ✅      | ✅         | ✅  | ✅  | ✅   |
 | Read USS file    | ✅      | ✅         | ✅  | ❌  | ❌   |
-| Read spool file  | ✅      | ✅         | ✅  | ❌  | ✅   |
+| Read spool file  | ✅      | ✅         | ✅  | ✅  | ✅   |
 | Write data set   | ✅      | ✅         | ✅  | ❌  | ❌   |
 | Write USS file   | ✅      | ✅         | ✅  | ❌  | ❌   |
-| Get job JCL      | ✅      | ✅         | ✅  | ❌  | ✅   |
-| List job status  | ✅      | ✅         | ✅  | ✅  | ❌   |
+| Get job JCL      | ✅      | ✅         | ✅  | ✅  | ✅   |
+| View job status  | ✅      | ✅         | ✅  | ✅  | ❌   |
 | Run console cmds | ✅      | ✅         | ✅  | ✅  | ❌   |
 | Run tso cmds     | ❌      | ❌         | ✅  | ❌  | ❌   |
 | Run unix cmds    | ❌      | ❌         | ✅  | ❌  | ❌   |
---
+++ packages/cli/src/list/spool-files/SpoolFiles.handler.ts
@@ -0,0 +1,29 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+import type { IHandlerParameters } from "@zowe/imperative";
+import type { ListSpools, ZSshClient } from "zowe-native-proto-sdk";
+import { SshBaseHandler } from "../../SshBaseHandler";
+
+export default class ListSpoolsHandler extends SshBaseHandler {
+    public async processWithClient(params: IHandlerParameters, client: ZSshClient): Promise<ListSpools.Response> {
+        const response = await client.jobs.listSpools({
+            jobId: params.arguments.jobId,
+        });
+        params.response.data.setMessage("Successfully listed %d spool files", response.items.length);
+        params.response.format.output({
+            output: response.items,
+            format: "table",
+            fields: ["id", "ddname", "stepname"],
+        });
+        return response;
+    }
+}
