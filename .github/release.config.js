5a0736ac90e00e90dc4c1fae592495922b77f945
Timothy Johnson <timothy.johnson@broadcom.com>
2025-03-14 08:34:10 -0500

    Add release workflow and run build workflows on push (#206)
    * Add push trigger and wait for zos-build
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Make zos-build workflow name unique
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Add release workflow and Octorelease configuration
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Fix cli and vsce publish
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    ---------
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ .github/release.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+    branches: [
+        {
+            name: "main",
+            level: "minor"
+        }
+    ],
+    plugins: [
+        [
+            "@octorelease/changelog",
+            {
+                headerLine: "## Recent Changes",
+            },
+            {
+                $cwd: "native",
+                headerLine: "## Recent Changes",
+            },
+        ],
+        [
+            "@octorelease/lerna",
+            {
+                // Use Lerna only for versioning and publish packages independently
+                npmPublish: false,
+            },
+        ],
+        [
+            "@octorelease/exec",
+            {
+                $cwd: "packages/cli",
+                dryRunAllow: ["publish"],
+                publishCmd: "npm run package",
+            },
+        ],
+        // TODO Replace exec plugin with the following once we're ready to publish to npm
+        // [
+        //     "@octorelease/npm",
+        //     {
+        //         $cwd: "packages/cli",
+        //         npmPublish: false,
+        //         tarballDir: "dist",
+        //     },
+        // ],
+        [
+            "@octorelease/vsce",
+            {
+                $cwd: "packages/vsce",
+                vscePublish: false,
+                vsixDir: "dist",
+            },
+        ],
+        [
+            "@octorelease/github",
+            {
+                assets: ["dist/*.pax.Z", "dist/*.tgz", "dist/*.vsix"],
+            },
+        ],
+        "@octorelease/git",
+    ]
+};
