e4d34dc4aac734bd58d8a28429ca1ca2d1d832a5
Jace Roell <111985297+jace-roell@users.noreply.github.com>
2025-04-03 16:07:06 -0400

    Update packages/vsce/src/Utilities.ts
    Co-authored-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
    Signed-off-by: Jace Roell <111985297+jace-roell@users.noreply.github.com>

--- packages/vsce/src/Utilities.ts
+++ packages/vsce/src/Utilities.ts
@@ -15,7 +15,7 @@ import type { SshSession } from "@zowe/zos-uss-for-zowe-sdk";
 import { Gui, ZoweVsCodeExtension, imperative } from "@zowe/zowe-explorer-api";
 import * as vscode from "vscode";
 import { ZSshUtils } from "zowe-native-proto-sdk";
-import { VscePromptApi } from "../../vsce/src/SshConfigUtils";
+import { VscePromptApi } from "./SshConfigUtils";
 import { SshClientCache } from "./SshClientCache";
 import { SshConfigUtils } from "./SshConfigUtils";
 
