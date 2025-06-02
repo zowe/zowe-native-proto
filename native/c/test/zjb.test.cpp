50766bbff0efa6a163c4f7df3935d27bbedc5f55
Dan Kelosky <dkelosky@gmail.com>
2025-06-02 11:14:09 -0400

    disable delete job
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- native/c/test/zjb.test.cpp
+++ native/c/test/zjb.test.cpp
@@ -25,18 +25,18 @@ void zjb_tests()
                   expect(zjb_list_by_owner(&zjb, owner, prefix, jobs)).ToBe(RTNCD_WARNING); // expect truncated list returned
                 });
 
-             //  it("should be able to submit JCL",
-             //     []() -> void
-             //     {
-             //       ZJB zjb = {0};
-             //       string jobid;
-             //       string jcl = "//IEFBR14$ JOB IZUACCT\n"
-             //                    "//RUNBR14  EXEC PGM=IEFBR14\n";
+             it("should be able to submit JCL",
+                []() -> void
+                {
+                  ZJB zjb = {0};
+                  string jobid;
+                  string jcl = "//IEFBR14$ JOB IZUACCT\n"
+                               "//RUNBR14  EXEC PGM=IEFBR14\n";
 
-             //       int rc = zjb_submit(&zjb, jcl, jobid);
-             //       expect(rc).ToBe(0);
-             //       expect(jobid).Not().ToBe("");
-             //     });
+                  int rc = zjb_submit(&zjb, jcl, jobid);
+                  expect(rc).ToBe(0);
+                  expect(jobid).Not().ToBe("");
+                });
 
              it("should be able to view a submitted job",
                 []() -> void
@@ -73,9 +73,11 @@ void zjb_tests()
 
              //       ZJob zjob;
 
+             //       string correlator = string(zjb.job_correlator, 64);
+
              //       memset(&zjb, 0, sizeof(zjb));
-             //       rc = zjb_delete_by_jobid(&zjb, "JOB00880", zjob);
-             //       expect(zjb_view(&zjb, jobid, zjob)).ToBe(0);
+             //       rc = zjb_delete_by_jobid(&zjb, correlator);
+             //       expect(rc).ToBe(0);
              //     });
            });
 }
