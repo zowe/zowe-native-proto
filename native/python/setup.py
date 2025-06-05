b763de1b9d1ebb101ab644eda2632c894e9f7606
Peizhao Mei <pem70@pitt.edu>
2025-06-05 13:58:32 -0400

    Python binding for uss_list
    Signed-off-by: Peizhao Mei <pem70@pitt.edu>

---
+++ native/python/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from distutils.core import setup, Extension
+import os
+import os.path
+
+
+lib_dir = os.path.abspath('../c/build-out')
+include_dir = os.path.abspath('../c/chdsect')
+
+zusf_py_module = Extension('_zusf_py',
+                           sources=['zusf_py_wrap.cxx', 'zusf_py.cpp', "../c/zusf.cpp", "../c/zut.cpp"],
+                           language='c++',
+                           extra_compile_args=['-q64', '-qascii'],
+                           include_dirs=[include_dir],
+                           libraries=['zut'],
+                           library_dirs=[lib_dir],
+                           extra_link_args=[
+                               '-q64', 
+                               '-G', 
+                               '-bexpall', 
+                               '-brtl',
+                           ],
+                           )
+
+setup (name = 'zusf_py',
+       version = '0.1',
+       author      = "SWIG Docs",
+       description = """Simple swig example for uss list""",
+       ext_modules = [zusf_py_module],
+       py_modules = ["zusf_py"],
+       )
