6ab9aea1d9113986dcc7768a3218f586d6a47d0c
Peizhao Mei <105866197+pem70@users.noreply.github.com>
2025-06-09 16:07:29 -0400

    Update native/python/zusf_py.cpp
    Co-authored-by: Trae Yelovich <trae.yelovich@broadcom.com>
    Signed-off-by: Peizhao Mei <105866197+pem70@users.noreply.github.com>

--- native/python/zusf_py.cpp
+++ native/python/zusf_py.cpp
@@ -7,8 +7,7 @@ std::string list_uss_dir(const std::string &path)
     std::string out;
     if (zusf_list_uss_file_path(&ctx, path.c_str(), out) != 0)
     {
-        std::string error_msg = ctx.diag.e_msg;
-        throw std::runtime_error(error_msg);
+        throw std::runtime_error(std::string(error_msg));
     }
     return out;
 }
