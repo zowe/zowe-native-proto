62f13f11bb6e768a101f8f5b571b16c3e305dd35
Andrew W. Harn <andrew.harn@broadcom.com>
2025-06-11 11:05:13 -0400

    Revert change to release config
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- .github/release.config.js
+++ .github/release.config.js
@@ -54,7 +54,7 @@ module.exports = {
         [
             "@octorelease/github",
             {
-                assets: process.env.TRAIN == "Release" ? ["dist/*.pax.Z", "dist/*.tgz", "dist/*.vsix"] : []
+                assets: ["dist/*.pax.Z", "dist/*.tgz", "dist/*.vsix"]
             },
         ],
         "@octorelease/git",
