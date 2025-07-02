544550862a9847122d0d6426c7b6bde6363a281b
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2025-07-02 11:36:38 -0400

    review: address comments re SIWG_LIB path
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- native/python/build.sh
+++ native/python/build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -e
 
+SWIG_PATH=$(type -p swig || true)
+if [ -z "$SWIG_PATH" ]; then
+  echo "ERROR: swig not found."
+  echo "Please install swig and re-run this script."
+  exit 1
+fi
+SWIG_DIR=$(dirname "$SWIG_PATH")
+# Use SWIG_LIB if it is set, otherwise assume it is in SWIG_DIR
+export SWIG_LIB=${SWIG_LIB:-$SWIG_DIR/swig_lib}
+
 export _BPXK_AUTOCVT=ON
 export _CEE_RUNOPTS="FILETAG(AUTOCVT,AUTOTAG) POSIX(ON)"
-export SWIG_LIB=/u/users/UTILS/swig_lib
 export _CC_CCMODE=1
 export _CXX_CCMODE=1
 export _C89_CCMODE=1
 export _CC_EXTRA_ARGS=1
 export _CXX_EXTRA_ARGS=1
 export _C89_EXTRA_ARGS=1
-
 
 swig -python -c++ zusf_py.i
 swig -python -c++ zjb_py.i
