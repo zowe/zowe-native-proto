4b55e315f40f42fa839cb12cbabbb144c356ed28
Dan Kelosky <dkelosky@gmail.com>
2025-07-08 15:04:55 -0400

    init
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

---
+++ examples/tests/runner.cpp
@@ -0,0 +1,39 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+#include <iostream>
+#include "ztest.hpp"
+#include "more.hpp"
+
+using namespace std;
+using namespace ztst;
+
+// @TEST TODO(Kelosky): fix this global variable
+std::string matcher = "";
+
+int main()
+{
+  return tests(
+      []() -> void
+      {
+        describe("test suite",
+                 []() -> void
+                 {
+                   it("test",
+                      [&]() -> void
+                      {
+                        Expect(1).ToBe(1);
+                      });
+
+                    more_tests();
+                 });
+      });
+}
