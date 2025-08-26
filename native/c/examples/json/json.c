b77fe54cffff8ec56b30e4bdd9f3097517fd57d3
Dan Kelosky <dkelosky@gmail.com>
2025-08-26 14:22:06 -0400

    initial version
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

---
+++ native/c/examples/json/json.c
@@ -0,0 +1,163 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+#include "zmetal.h"
+#include "zwto.h"
+#include "zjsonm.h"
+
+#pragma prolog(main, " ZWEPROLG NEWDSA=(YES,128) ")
+#pragma epilog(main, " ZWEEPILG ")
+
+int main()
+{
+  PARSE_HANDLE handle = {0};
+  KEY_HANDLE key_handle = {0};
+  DIAG diag = {0};
+
+  int rc = 0;
+  char json[] = "{\"name\": \"John\", \"isMarried\": true, \"hasKids\": false, \"age\": 30, \"address\": {\"street\": \"123 Main St\", \"city\": \"Anytown\", \"state\": \"CA\", \"zip\": \"12345\"}}";
+
+  zwto_debug("@TEST json: %s", json);
+
+  // initialize JSON services
+  rc = ZJSNMINIT(&handle, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSNMINIT error: %d", rc);
+  }
+
+  // parse JSON
+  rc = ZJSMPARS(&handle, json, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMPARS error: %d", rc);
+  }
+
+  // search for string key
+  char *PTR32 string_key = "name";
+  rc = ZJSMSRCH(&handle, string_key, &key_handle, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMSRCH error: %d", rc);
+  }
+
+  // get type of key
+  // NOTE(Kelosky): types are:
+  // HWTJ_OBJECT_TYPE
+  // HWTJ_ARRAY_TYPE
+  // HWTJ_STRING_TYPE
+  // HWTJ_NUMBER_TYPE
+  // HWTJ_BOOLEAN_TYPE
+  // HWTJ_NULL_TYPE
+  int type = 0;
+  rc = ZJSNGJST(&handle, &key_handle, &type, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSNGJST error: %d", rc);
+  }
+  zwto_debug("@TEST type of name: %d", type);
+
+  // get value from previous search
+  char *PTR32 string_value = NULL;
+  int string_value_length = 0;
+  rc = ZJSMGVAL(&handle, &key_handle, &string_value, &string_value_length, &diag);
+
+  zwto_debug("@TEST result: %.*s", string_value_length, string_value);
+
+  // search for object key
+  int number_entries = 0;
+  char *PTR32 object_key = "address";
+  rc = ZJSMSRCH(&handle, object_key, &key_handle, &diag);
+
+  // get number of entries
+  rc = ZJSMGNUE(&handle, &key_handle, &number_entries, &diag);
+  zwto_debug("@TEST number_entries: %d", number_entries);
+
+  // search for boolean key
+  char *PTR32 boolean_key = "isMarried";
+  rc = ZJSMSRCH(&handle, boolean_key, &key_handle, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMSRCH error: %d", rc);
+  }
+
+  rc = ZJSNGJST(&handle, &key_handle, &type, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSNGJST error: %d", rc);
+  }
+  zwto_debug("@TEST type of isMarried: %d", type);
+
+  // get boolean value
+  char boolean_value = 0x00;
+  rc = ZJSMGBOV(&handle, &key_handle, &boolean_value, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMGBOV error: %d", rc);
+  }
+  zwto_debug("@TEST boolean_value result: %x", boolean_value);
+
+  // search for boolean key
+  char *PTR32 boolean_key2 = "hasKids";
+  rc = ZJSMSRCH(&handle, boolean_key2, &key_handle, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMSRCH error: %d", rc);
+  }
+
+  rc = ZJSNGJST(&handle, &key_handle, &type, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSNGJST error: %d", rc);
+  }
+  zwto_debug("@TEST type of hasKids: %d", type);
+
+  // get boolean value
+  char boolean_value2 = 0x00;
+  rc = ZJSMGBOV(&handle, &key_handle, &boolean_value2, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMGBOV error: %d", rc);
+  }
+  zwto_debug("@TEST boolean_value2 result: %x", boolean_value2);
+
+  char *PTR32 boolean_value3 = NULL;
+  int boolean_value3_length = 0;
+  rc = ZJSMGVAL(&handle, &key_handle, &boolean_value3, &boolean_value3_length, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMGVAL error: %d", rc);
+  }
+  zwto_debug("@TEST boolean_value3 result: %.*s", boolean_value3_length, boolean_value3);
+
+  // search for number key
+  char *PTR32 number_key = "age";
+  rc = ZJSMSRCH(&handle, number_key, &key_handle, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMSRCH error: %d", rc);
+  }
+
+  // get value from previous search
+  char *PTR32 number_value = NULL;
+  int number_value_length = 0;
+  rc = ZJSMGVAL(&handle, &key_handle, &number_value, &number_value_length, &diag);
+  if (0 != rc)
+  {
+    zwto_debug("@TEST ZJSMGVAL error: %d", rc);
+  }
+  zwto_debug("@TEST result: %.*s", number_value_length, number_value);
+
+  // terminate JSON services
+  rc = ZJSNMTERM(&handle, &diag);
+
+  return 0;
+}
---
+++ native/c/zjsonm.h
@@ -0,0 +1,174 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+#ifndef ZJSONM_H
+#define ZJSONM_H
+
+#include <hwtjic.h> // ensure to include /usr/include
+#include "ztype.h"
+#include "zmetal.h"
+#include "zwto.h"
+
+typedef struct
+{
+  char x[12];
+} PARSE_HANDLE;
+
+typedef struct
+{
+  int x;
+} KEY_HANDLE;
+
+typedef struct
+{
+  char msg[132];
+} DIAG;
+
+typedef int (*HWTJINIT)(int *PTR32, int *PTR32, PARSE_HANDLE *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJTERM)(int *PTR32, PARSE_HANDLE *PTR32, int *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJPARS)(int *PTR32, PARSE_HANDLE *PTR32, char *PTR32 *PTR32, int *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJSRCH)(int *PTR32, PARSE_HANDLE *PTR32, int *PTR32, char *PTR32 *PTR32, int *PTR32, int *PTR32, int *PTR32, KEY_HANDLE *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJGJST)(int *PTR32, PARSE_HANDLE *PTR32, KEY_HANDLE *PTR32, int *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJGNUE)(int *PTR32, PARSE_HANDLE *PTR32, KEY_HANDLE *PTR32, int *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJGVAL)(int *PTR32, PARSE_HANDLE *PTR32, KEY_HANDLE *PTR32, char *PTR32 *PTR32, int *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+typedef int (*HWTJGBOV)(int *PTR32, PARSE_HANDLE *PTR32, KEY_HANDLE *PTR32, char *PTR32, DIAG *PTR32) ATTRIBUTE(amode31);
+
+// https://www.ibm.com/docs/en/zos/3.1.0?topic=parser-hwtjinit-initialize-instance
+static int ZJSNMINIT(PARSE_HANDLE *PTR32 handle, DIAG *PTR32 diag)
+{
+  HWTJINIT hwtjinit = (HWTJINIT)load_module31("HWTJINIT");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  int max_size = 0;
+
+  hwtjinit(&rc, &max_size, handle, diag_p);
+  delete_module("HWTJINIT");
+  return rc;
+}
+
+// https://www.ibm.com/docs/en/zos/3.1.0?topic=parser-hwtjpars-parse-json-string
+// TODO(Kelosky): get return code EQUs
+static int ZJSMPARS(PARSE_HANDLE *PTR32 handle, char *PTR32 json, DIAG *PTR32 diag)
+{
+  HWTJPARS hwtjpars = (HWTJPARS)load_module31("HWTJPARS");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  int json_file_size = (int)strlen(json);
+  hwtjpars(&rc, handle, &json, &json_file_size, diag_p);
+  delete_module("HWTJPARS");
+  return rc;
+}
+
+// https://www.ibm.com/docs/en/zos/3.1.0?topic=parser-hwtjsrch-search
+static int ZJSMSRCH(PARSE_HANDLE *PTR32 handle, char *PTR32 key, KEY_HANDLE *PTR32 key_handle, DIAG *PTR32 diag)
+{
+  HWTJSRCH hwtjsrch = (HWTJSRCH)load_module31("HWTJSRCH");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  int search_type = HWTJ_SEARCHTYPE_GLOBAL;
+  int object_handle = 0;
+  int starting_handle = 0;
+  char *PTR32 key_p = key;
+  int name_length = (int)strlen(key);
+  int force = 0; // TODO(Kelosky): get return code EQUs
+
+  hwtjsrch(&rc, handle, &search_type, &key_p, &name_length, &object_handle, &starting_handle, key_handle, diag_p);
+  delete_module("HWTJSRCH");
+  return rc;
+}
+
+static int ZJSNGJST(PARSE_HANDLE *PTR32 handle, KEY_HANDLE *PTR32 key_handle, int *PTR32 type, DIAG *PTR32 diag)
+{
+  HWTJGJST hwtjgjst = (HWTJGJST)load_module31("HWTJGJST");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  hwtjgjst(&rc, handle, key_handle, type, diag_p);
+  delete_module("HWTJGJST");
+  return rc;
+}
+
+static int ZJSMGVAL(PARSE_HANDLE *PTR32 handle, KEY_HANDLE *PTR32 key_handle, char *PTR32 *PTR32 value, int *PTR32 value_length, DIAG *PTR32 diag)
+{
+  HWTJGVAL hwtjgval = (HWTJGVAL)load_module31("HWTJGVAL");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  hwtjgval(&rc, handle, key_handle, value, value_length, diag_p);
+  delete_module("HWTJGVAL");
+  return rc;
+}
+
+static int ZJSMGNUE(PARSE_HANDLE *PTR32 handle, KEY_HANDLE *PTR32 key_handle, int *PTR32 number_entries, DIAG *PTR32 diag)
+{
+  HWTJGNUE hwtjgnue = (HWTJGNUE)load_module31("HWTJGNUE");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  hwtjgnue(&rc, handle, key_handle, number_entries, diag_p);
+  delete_module("HWTJGNUE");
+  return rc;
+}
+
+static int ZJSMGBOV(PARSE_HANDLE *PTR32 handle, KEY_HANDLE *PTR32 key_handle, char *PTR32 value, DIAG *PTR32 diag)
+{
+  HWTJGBOV hwtjgbov = (HWTJGBOV)load_module31("HWTJGBOV");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  hwtjgbov(&rc, handle, key_handle, value, diag_p);
+
+  delete_module("HWTJGBOV");
+  return rc;
+}
+
+// https://www.ibm.com/docs/en/zos/3.1.0?topic=parser-hwtjterm-terminate-instance
+static int ZJSNMTERM(PARSE_HANDLE *PTR32 handle, DIAG *PTR32 diag)
+{
+  HWTJTERM hwtjterm = (HWTJTERM)load_module31("HWTJTERM");
+  // TODO(Kelosky): add error handling
+
+  int rc = 0;
+  DIAG *PTR32 diag_p = diag;
+  diag_p = (DIAG * PTR32)((unsigned int)diag_p | 0x80000000);
+
+  int force = 0; // TODO(Kelosky): get return code EQUs
+
+  hwtjterm(&rc, handle, &force, diag_p);
+  delete_module("HWTJTERM");
+  return rc;
+}
+
+#endif
