f0e30858d94bcee4ae85d5949d6aa9a7091ba3a5
Timothy Johnson <timothy.johnson@broadcom.com>
2025-09-08 12:15:59 -0400

    Add license headers to C++ types and refactor type generation script
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- native/c/types/common.h
+++ native/c/types/common.h
@@ -1,3 +1,14 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
 #ifndef COMMON_TYPES_H
 #define COMMON_TYPES_H
 
@@ -15,7 +26,6 @@ struct RpcNotification {
 };
 
 struct RpcRequest : public RpcNotification {
-    void* params; // optional
     // int
     int id;
 };
