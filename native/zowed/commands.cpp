74ef903d5a0ea734e678d17f15a7a15a525c394c
Timothy Johnson <timothy.johnson@broadcom.com>
2025-10-04 08:02:20 -0400

    Extract common argument mappings into reusable CommandBuilder functions
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- native/zowed/commands.cpp
+++ native/zowed/commands.cpp
@@ -15,40 +15,49 @@
 #include "../c/commands/job.hpp"
 #include "../c/commands/uss.hpp"
 
+// Helper functions to create builders with common argument mappings
+static CommandBuilder create_ds_builder(CommandBuilder::CommandHandler handler)
+{
+  return CommandBuilder(handler).rename_arg("dsname", "dsn");
+}
+
+static CommandBuilder create_job_builder(CommandBuilder::CommandHandler handler)
+{
+  return CommandBuilder(handler).rename_arg("job-id", "jobid");
+}
+
+static CommandBuilder create_uss_builder(CommandBuilder::CommandHandler handler)
+{
+  return CommandBuilder(handler).rename_arg("fspath", "file-path");
+}
+
 void register_ds_commands(CommandDispatcher &dispatcher)
 {
   dispatcher.register_command("createDataset",
-                              CommandBuilder(ds::create_with_attributes)
-                                  .rename_arg("dsname", "dsn")
+                              create_ds_builder(ds::create_with_attributes)
                                   .flatten_obj("attributes"));
   dispatcher.register_command("createMember",
-                              CommandBuilder(ds::handle_data_set_create_member)
-                                  .rename_arg("dsname", "dsn"));
+                              create_ds_builder(ds::handle_data_set_create_member));
   dispatcher.register_command("deleteDataset",
-                              CommandBuilder(ds::handle_data_set_delete)
-                                  .rename_arg("dsname", "dsn"));
+                              create_ds_builder(ds::handle_data_set_delete));
   dispatcher.register_command("listDatasets",
                               CommandBuilder(ds::handle_data_set_list)
                                   .rename_arg("pattern", "dsn")
                                   .set_default("warn", false));
   dispatcher.register_command("listDsMembers",
-                              CommandBuilder(ds::handle_data_set_list_members)
-                                  .rename_arg("dsname", "dsn")
+                              create_ds_builder(ds::handle_data_set_list_members)
                                   .set_default("warn", false));
   dispatcher.register_command("readDataset",
-                              CommandBuilder(ds::handle_data_set_view)
-                                  .rename_arg("dsname", "dsn")
+                              create_ds_builder(ds::handle_data_set_view)
                                   .rename_arg("volume", "volser")
                                   .set_default("encoding", "IBM-1047")
                                   .set_default("return-etag", true)
                                   .read_stdout("data", true)
                                   .handle_fifo("stream", "pipe-path", FifoMode::GET));
   dispatcher.register_command("restoreDataset",
-                              CommandBuilder(ds::handle_data_set_restore)
-                                  .rename_arg("dsname", "dsn"));
+                              create_ds_builder(ds::handle_data_set_restore));
   dispatcher.register_command("writeDataset",
-                              CommandBuilder(ds::handle_data_set_write)
-                                  .rename_arg("dsname", "dsn")
+                              create_ds_builder(ds::handle_data_set_write)
                                   .rename_arg("volume", "volser")
                                   .set_default("encoding", "IBM-1047")
                                   .write_stdin("data", true)
@@ -58,83 +67,65 @@ void register_ds_commands(CommandDispatcher &dispatcher)
 void register_job_commands(CommandDispatcher &dispatcher)
 {
   dispatcher.register_command("cancelJob",
-                              CommandBuilder(job::handle_job_cancel)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_cancel));
   dispatcher.register_command("deleteJob",
-                              CommandBuilder(job::handle_job_delete)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_delete));
   dispatcher.register_command("getJcl",
-                              CommandBuilder(job::handle_job_view_jcl)
-                                  .rename_arg("job-id", "jobid")
+                              create_job_builder(job::handle_job_view_jcl)
                                   .read_stdout("data", false));
   dispatcher.register_command("getJobStatus",
-                              CommandBuilder(job::handle_job_view_status)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_view_status));
   dispatcher.register_command("holdJob",
-                              CommandBuilder(job::handle_job_hold)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_hold));
   dispatcher.register_command("listJobs",
                               CommandBuilder(job::handle_job_list)
                                   .set_default("warn", false));
   dispatcher.register_command("listSpools",
-                              CommandBuilder(job::handle_job_list_files)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_list_files));
   dispatcher.register_command("readSpool",
-                              CommandBuilder(job::handle_job_view_file)
-                                  .rename_arg("job-id", "jobid")
+                              create_job_builder(job::handle_job_view_file)
                                   .rename_arg("spool-id", "key")
                                   .set_default("encoding", "IBM-1047")
                                   .read_stdout("data", true));
   dispatcher.register_command("releaseJob",
-                              CommandBuilder(job::handle_job_release)
-                                  .rename_arg("job-id", "jobid"));
+                              create_job_builder(job::handle_job_release));
   dispatcher.register_command("submitJcl",
                               CommandBuilder(job::handle_job_submit_jcl)
                                   .write_stdin("jcl", true));
   dispatcher.register_command("submitJob",
-                              CommandBuilder(job::handle_job_submit)
-                                  .rename_arg("dsname", "dsn"));
+                              create_ds_builder(job::handle_job_submit));
   dispatcher.register_command("submitUss",
-                              CommandBuilder(job::handle_job_submit_uss)
-                                  .rename_arg("fspath", "file-path"));
+                              create_uss_builder(job::handle_job_submit_uss));
 }
 
 void register_uss_commands(CommandDispatcher &dispatcher)
 {
   dispatcher.register_command("chmodFile",
-                              CommandBuilder(uss::handle_uss_chmod)
-                                  .rename_arg("fspath", "file-path"));
+                              create_uss_builder(uss::handle_uss_chmod));
   dispatcher.register_command("chownFile",
-                              CommandBuilder(uss::handle_uss_chown)
-                                  .rename_arg("fspath", "file-path"));
+                              create_uss_builder(uss::handle_uss_chown));
   dispatcher.register_command("chtagFile",
-                              CommandBuilder(uss::handle_uss_chtag)
-                                  .rename_arg("fspath", "file-path"));
+                              create_uss_builder(uss::handle_uss_chtag));
   const auto handle_uss_create = [](plugin::InvocationContext &context) -> int
   {
     auto handler = context.get<bool>("is-dir", false) ?
       uss::handle_uss_create_dir : uss::handle_uss_create_file;
     return handler(context); };
   dispatcher.register_command("createFile",
-                              CommandBuilder(handle_uss_create)
-                                  .rename_arg("fspath", "file-path")
+                              create_uss_builder(handle_uss_create)
                                   .rename_arg("permissions", "mode"));
   dispatcher.register_command("deleteFile",
-                              CommandBuilder(uss::handle_uss_delete)
-                                  .rename_arg("fspath", "file-path"));
+                              create_uss_builder(uss::handle_uss_delete));
   dispatcher.register_command("listFiles",
-                              CommandBuilder(uss::handle_uss_list)
-                                  .rename_arg("fspath", "file-path")
+                              create_uss_builder(uss::handle_uss_list)
                                   .set_default("response-format-csv", true));
   dispatcher.register_command("readFile",
-                              CommandBuilder(uss::handle_uss_view)
-                                  .rename_arg("fspath", "file-path")
+                              create_uss_builder(uss::handle_uss_view)
                                   .set_default("encoding", "IBM-1047")
                                   .read_stdout("data", true)
                                   .handle_fifo("stream", "pipe-path", FifoMode::GET, true));
   dispatcher.register_command("writeFile",
-                              CommandBuilder(uss::handle_uss_write)
-                                  .rename_arg("fspath", "file-path")
+                              create_uss_builder(uss::handle_uss_write)
                                   .set_default("encoding", "IBM-1047")
                                   .write_stdin("data", true)
                                   .handle_fifo("stream", "pipe-path", FifoMode::PUT));
