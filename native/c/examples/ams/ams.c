b9d654da642744540c4f564c8bc8bc521657a81c
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2025-10-21 09:33:07 -0400

    ams example
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

---
+++ native/c/examples/ams/ams.c
@@ -0,0 +1,10 @@
+// #include "zmetal.h"
+#include "zwto.h"
+
+#pragma prolog(main, " ZWEPROLG NEWDSA=(YES,128) ")
+#pragma epilog(main, " ZWEEPILG ")
+int main()
+{
+  zwto_debug("ams started");
+  return 0;
+}
