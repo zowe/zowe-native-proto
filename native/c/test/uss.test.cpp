fd0e25b674c3c2c894dcd6f321fa6f954f5c7918
jace-roell <jace.roell@hotmail.com>
2025-10-29 11:31:03 -0400

    chmod tests
    Signed-off-by: jace-roell <jace.roell@hotmail.com>

--- native/c/test/uss.test.cpp
+++ native/c/test/uss.test.cpp
@@ -21,6 +21,7 @@ using namespace std;
 using namespace ztst;
 
 const string zowex_command = "./../build-out/zowex";
+const string ussTestDir = "/tmp";
 
 void uss_tests()
 {
@@ -33,15 +34,15 @@ void uss_tests()
                         it("should create a uss file",
                            []() -> void
                            {
-                             int rc = 0;
-                             string uss_file = get_random_uss("/tmp");
-                             string response;
-                             string command = zowex_command + " uss create-file " + uss_file;
-                             //  string command = zowex_command + " uss create-h";
+                             //  int rc = 0;
+                             //  string uss_file = get_random_uss("/tmp/zowex_test");
+                             //  string response;
+                             //  string command = zowex_command + " uss create-file " + uss_file;
+                             //  //  string command = zowex_command + " uss create-h";
 
-                             rc = execute_command_with_output(command, response);
-                             ExpectWithContext(rc, response).ToBe(0);
-                             Expect(response).ToContain("USS file '" + uss_file + "' created");
+                             //  rc = execute_command_with_output(command, response);
+                             //  ExpectWithContext(rc, response).ToBe(0);
+                             //  Expect(response).ToContain("USS file '" + uss_file + "' created");
 
                              //  command = zowex_command + " uss list " + uss_file + " -a --rfc";
                              //  rc = execute_command_with_output(command, response);
@@ -52,16 +53,77 @@ void uss_tests()
                              //  ExpectWithContext(rc, response).ToBe(0);
                              //  Expect(response).ToContain("Uss file '" + uss_file + "' deleted");
                            });
-
-                        it("handle_uss_create_file1",
+                      });
+             describe("chmod",
+                      []() -> void
+                      {
+                        it("should properly chmod a file",
                            []() -> void
                            {
-                             int test1 = 1 + 2;
-                             int test2 = 3;
-                             Expect(test1).ToBe(test2);
+                             int rc = 0;
+                             string uss_file = get_random_uss(ussTestDir);
+                             string response;
+                             string command = zowex_command + " uss create-file " + uss_file;
+                             string getPermissionsCommand = "ls -l " + uss_file;
+                             string chmodFileCommand = zowex_command + " uss chmod 777 " + uss_file;
+
+                             // Create file
+                             rc = execute_command_with_output(command, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain("USS file '" + uss_file + "' created");
+
+                             // Get Default Perms
+                             rc = execute_command_with_output(getPermissionsCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain(("-rw-r--r--"));
+
+                             // chmod 777 the test file and get permissions
+                             rc = execute_command_with_output(chmodFileCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             rc = execute_command_with_output(getPermissionsCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain(("-rwxrwxrwx"));
+                           });
+                        it("should properly chmod a directory",
+                           []() -> void
+                           {
+                             int rc = 0;
+                             string uss_dir = get_random_uss(ussTestDir) + "_dir";
+                             string response;
+                             string command = zowex_command + " uss create-dir " + uss_dir;
+                             string getPermissionsCommand = "ls -ld " + uss_dir;
+                             string chmodFileCommand = zowex_command + " uss chmod 777 " + uss_dir + " -r";
+
+                             // Create directory
+                             rc = execute_command_with_output(command, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain("USS directory '" + uss_dir + "' created");
+
+                             // Get default directory perms
+                             rc = execute_command_with_output(getPermissionsCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain(("drwxr-xr-x"));
+
+                             // `chmod 777 -r` the test directory and get permissions
+                             rc = execute_command_with_output(chmodFileCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             rc = execute_command_with_output(getPermissionsCommand, response);
+                             ExpectWithContext(rc, response).ToBe(0);
+                             Expect(response).ToContain(("drwxrwxrwx"));
+                           });
+                        it("should properly handle calling chmod on a file that does not exist",
+                           []() -> void
+                           {
+                             int rc = 0;
+                             string uss_file = ussTestDir + "/test_does_not_exist";
+                             string response;
+                             string chmodFileCommand = zowex_command + " uss chmod 777 " + uss_file;
+
+                             // chmod 777 the test file and get permissions
+                             rc = execute_command_with_output(chmodFileCommand, response);
+                             ExpectWithContext(rc, response).ToBe(1);
+                             Expect(response).ToContain("Path `" + uss_file + "` does not exist");
                            });
                       });
-             describe("create",
-                      []() -> void {});
            });
 }
