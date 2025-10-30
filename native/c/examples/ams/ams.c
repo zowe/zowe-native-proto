3e78e962c56ec5f985950a589f3d47b2c5344fa7
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2025-10-30 07:26:55 -0400

    open without type=j
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

--- native/c/examples/ams/ams.c
+++ native/c/examples/ams/ams.c
@@ -17,6 +17,10 @@
 #include "zmetal.h"
 #include "zdbg.h"
 
+// TODO(Kelosky): determine if we need to use BPAM up front
+// if BPAM, do dynalloc, RDJFCB to valid attributes or perhaps use something like fldata
+// open without TYPE=J
+
 #pragma prolog(AMSMAIN, " ZWEPROLG NEWDSA=(YES,256) ")
 #pragma epilog(AMSMAIN, " ZWEEPILG ")
 int AMSMAIN()
@@ -37,28 +41,28 @@ int AMSMAIN()
   zwto_debug("@TEST ioc->jfcb.jfcnlrec: %d", ioc->jfcb.jfcnlrec);
   zwto_debug("@TEST ioc->jfcb.jfcbaxbf: %d", ioc->jfcb.jfcbaxbf); // JFCBLKSI
 
-  ZAMS24_FUNCS funcs = {0};
-  AMS24_fn AMS24 = (AMS24_fn)load_module31("ZAMS24");
-  if (!AMS24)
-  {
-    zwto_debug("@TEST AMS24 not found");
-    s0c3_abend(1);
-  }
+  // ZAMS24_FUNCS funcs = {0};
+  // AMS24_fn AMS24 = (AMS24_fn)load_module31("ZAMS24");
+  // if (!AMS24)
+  // {
+  //   zwto_debug("@TEST AMS24 not found");
+  //   s0c3_abend(1);
+  // }
 
-  rc = AMS24(&funcs);
-  if (0 != rc)
-  {
-    zwto_debug("@TEST AMS24 failed: %d", rc);
-    s0c3_abend(1);
-  }
+  // rc = AMS24(&funcs);
+  // if (0 != rc)
+  // {
+  //   zwto_debug("@TEST AMS24 failed: %d", rc);
+  //   s0c3_abend(1);
+  // }
 
+  rc = open_input(&ioc->dcb);
   // rc = funcs.open_input_j(ioc);
-  rc = funcs.open_input_j(ioc);
-  if (0 != rc)
-  {
-    zwto_debug("@TEST open_input_j failed: %d", rc);
-    s0c3_abend(1);
-  }
+  // if (0 != rc)
+  // {
+  //   zwto_debug("@TEST open_input_j failed: %d", rc);
+  //   s0c3_abend(1);
+  // }
 
   return 0;
 
