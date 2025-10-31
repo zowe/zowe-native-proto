ec693c6a9df5b44c4a2249240e75f54c639f3851
Timothy Johnson <timothy.johnson@broadcom.com>
2025-10-31 16:31:27 -0400

    Remove redirection check for stdin
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- native/c/commands/uss.cpp
+++ native/c/commands/uss.cpp
@@ -340,20 +340,7 @@ int handle_uss_write(InvocationContext &context)
     {
       std::istreambuf_iterator<char> begin(context.input_stream());
       std::istreambuf_iterator<char> end;
-
-      if (!context.is_redirecting_input())
-      {
-        vector<char> input(begin, end);
-        const auto temp = string(input.begin(), input.end());
-        input.clear();
-        const auto bytes = zut_get_contents_as_bytes(temp);
-
-        data.assign(bytes.begin(), bytes.end());
-      }
-      else
-      {
-        data.assign(begin, end);
-      }
+      data.assign(begin, end);
     }
     else
     {
