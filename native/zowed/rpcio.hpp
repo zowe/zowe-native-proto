16f4b13641f6aeea3781dc9fab84132ee38f0af3
Timothy Johnson <timothy.johnson@broadcom.com>
2025-11-14 10:43:43 -0500

    zowed: Refactor validator and move RPC types to optimize compilation (#667)
    * Create validator.cpp and use unordered map
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Move rpc types into rpcio.hpp
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Reduce overhead of validator macros
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Move unnecessary include and revert unordered map
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Restore the ValidationResult struct
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Clean up validator macros
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Remove unnecessary NestedSchemaInfo struct
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    * Enforce max depth of 1 for schema validation
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>
    
    ---------
    
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- native/zowed/rpcio.hpp
+++ native/zowed/rpcio.hpp
@@ -13,11 +13,59 @@
 #define RPCIO_HPP
 
 #include "../c/extend/plugin.hpp"
+#include "../c/zjson.hpp"
 #include "../c/zstd.hpp"
 #include <sstream>
 
-// Forward declaration
-struct RpcNotification;
+// JSON-RPC 2.0 Standard Error Codes
+namespace RpcErrorCode
+{
+enum
+{
+  PARSE_ERROR = -32700,      // Invalid JSON was received
+  INVALID_REQUEST = -32600,  // The JSON sent is not a valid Request object
+  METHOD_NOT_FOUND = -32601, // The method does not exist / is not available
+  INVALID_PARAMS = -32602,   // Invalid method parameter(s)
+  INTERNAL_ERROR = -32603,   // Internal JSON-RPC error
+  // -32000 to -32099 are reserved for implementation-defined server-errors
+  REQUEST_TIMEOUT = -32001 // Request exceeded timeout limit
+};
+}
+
+struct RpcNotification
+{
+  std::string jsonrpc;
+  std::string method;
+  zstd::optional<zjson::Value> params;
+};
+ZJSON_DERIVE(RpcNotification, jsonrpc, method, params);
+
+struct RpcRequest : RpcNotification
+{
+  int id;
+};
+ZJSON_DERIVE(RpcRequest, jsonrpc, method, params, id);
+
+struct ErrorDetails
+{
+  int code;
+  std::string message;
+  zstd::optional<zjson::Value> data;
+};
+ZJSON_DERIVE(ErrorDetails, code, message, data);
+
+struct RpcResponse
+{
+  std::string jsonrpc;
+  zstd::optional<zjson::Value> result;
+  zstd::optional<ErrorDetails> error;
+  zstd::optional<int> id;
+};
+ZJSON_SERIALIZABLE(RpcResponse,
+                   ZJSON_FIELD(RpcResponse, jsonrpc),
+                   ZJSON_FIELD(RpcResponse, result).skip_serializing_if_none(),
+                   ZJSON_FIELD(RpcResponse, error).skip_serializing_if_none(),
+                   ZJSON_FIELD(RpcResponse, id));
 
 static constexpr size_t LARGE_DATA_THRESHOLD = 16 * 1024 * 1024; // 16MB
 
