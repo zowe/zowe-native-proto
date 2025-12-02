058f246d823d6a0237e419b7cc52a23e545e932e
Pujal <pujal.gandhi@broadcom.com>
2025-12-02 15:13:30 -0500

    updated tso and console tests
    Signed-off-by: Pujal <pujal.gandhi@broadcom.com>

---
+++ native/c/test/zowex.tso.test.cpp
@@ -0,0 +1,67 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ *
+ */
+
+#include <cstddef>
+#include <ctime>
+#include <stdlib.h>
+#include <string>
+#include "ztest.hpp"
+#include "zutils.hpp"
+
+using namespace std;
+using namespace ztst;
+
+const string zowex_command = "./../build-out/zowex";
+void zowex_tso_tests()
+{
+  describe("tso", [&]() -> void
+           {
+             it("should display help", []() -> void
+                {
+            string response;
+            string command = zowex_command + " tso issue command";
+            int rc = execute_command_with_output(command, response);
+
+            ExpectWithContext(rc, response).ToBe(0);
+            Expect(response).ToContain("issue");
+            Expect(response).ToContain("help"); });
+
+             it("should successfully issue a simple TSO command", []() -> void
+                {
+            string response;
+            string command = zowex_command + " tso issue command status";
+            int rc = execute_command_with_output(command, response);
+
+            ExpectWithContext(rc, response).ToBe(0);
+            Expect(response).ToContain("status"); });
+
+             it("should fail when issuing a command with an invalid account", []() -> void
+                {
+            string response;
+            string command = zowex_command + " tso issue command status --account ACCOUNT123";
+            int rc = execute_command_with_output(command, response);
+
+            ExpectWithContext(rc, response).Not().ToBe(0);
+            Expect(response).ToContain("Error");
+            Expect(response).ToContain("ACCOUNT123"); });
+
+             it("should error when the TSO command itself is invalid", []() -> void
+                {
+            string response;
+            string command = zowex_command + " tso issue \"invalidCommand\"";
+
+            int rc = execute_command_with_output(command, response);
+
+            ExpectWithContext(rc, response).Not().ToBe(0);
+            Expect(response).ToContain("Error");
+            Expect(response).ToContain("invalidCommand"); });
+           });
+}
