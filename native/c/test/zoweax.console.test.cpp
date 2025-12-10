191a112bfc5b8bca574ee108a38550c356a63f5f
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2025-12-10 14:46:36 -0500

    test: fix some system tests
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- native/c/test/zoweax.console.test.cpp
+++ native/c/test/zoweax.console.test.cpp
@@ -22,7 +22,7 @@ void zoweax_console_tests()
         it("should display help", []() -> void
         {
             string response;
-            string command = zowex_command + " console";
+            string command = zoweax_command + " console";
             int rc = execute_command_with_output(command, response);
 
             ExpectWithContext(rc, response).ToBe(0);
@@ -32,7 +32,7 @@ void zoweax_console_tests()
         it("should issue console command successfully", []() -> void
         {
             string response;
-            string command = zowex_command + " console issue \"D T\"";
+            string command = zoweax_command + " console issue \"D T\"";
             int rc = execute_command_with_output(command, response);
 
             ExpectWithContext(rc, response).ToBe(0);
@@ -42,7 +42,7 @@ void zoweax_console_tests()
         it("should error when the console name is invalid", []() -> void
         {
             string response;
-            string command = zowex_command +
+            string command = zoweax_command +
                             " console issue \"D IPLINFO\" --console-name 1Invalid";
             int rc = execute_command_with_output(command, response);
 
@@ -53,7 +53,7 @@ void zoweax_console_tests()
         it("should successfully issue a command when the console does not already exist", []() -> void
         {
             string response;
-            string command = zowex_command +
+            string command = zoweax_command +
                             " console issue \"D IPLINFO\" --console-name newConsoleName";
             int rc = execute_command_with_output(command, response);
 
@@ -64,7 +64,7 @@ void zoweax_console_tests()
         it("should issue without waiting when boolean is set to false", []() -> void
         {
             string response;
-            string command = zowex_command + " console issue \"D IPLINFO\" --wait false";
+            string command = zoweax_command + " console issue \"D IPLINFO\" --wait false";
             int rc = execute_command_with_output(command, response);
 
             ExpectWithContext(rc, response).ToBe(0);
@@ -73,8 +73,7 @@ void zoweax_console_tests()
         it("should fail when using a non-APF authorized binary", []() -> void
         {
             string response;
-            string nonAPF = "./../build-out/zowex";
-            string command = nonAPF + " console issue \"D T\"";
+            string command = zowex_command + " console issue \"D T\"";
             int rc = execute_command_with_output(command, response);
 
             ExpectWithContext(rc, response).Not().ToBe(0);
--- native/c/test/zutils.hpp
+++ native/c/test/zutils.hpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <vector>
 const std::string zowex_command = "./../build-out/zowex";
+const std::string zoweax_command = "./../build-out/zoweax";
 int execute_command_with_input(const std::string &command, const std::string &input, bool suppress_output = true);
 int execute_command_with_output(const std::string &command, std::string &output);
 std::string get_random_string(const int length = 7, const bool allNumbers = true);
