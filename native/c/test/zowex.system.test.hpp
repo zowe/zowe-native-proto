04dbb649467760a73ca29bfc7dc0b07bf07e6423
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2026-03-13 14:33:10 -0400

    revert merge delete failure
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

--- native/c/test/zowex.system.test.hpp
+++ native/c/test/zowex.system.test.hpp
@@ -9,15 +9,7 @@
  *
  */
 
-<<<<<<<< HEAD:native/c/test/zowex.system.test.hpp
 #ifndef ZOWEX_SYSTEM_TEST_HPP
 #define ZOWEX_SYSTEM_TEST_HPP
 void zowex_system_tests();
-========
-#ifndef WORKER_TEST_HPP
-#define WORKER_TEST_HPP
-
-void worker_tests();
-
->>>>>>>> main:native/c/test/worker.test.hpp
 #endif
