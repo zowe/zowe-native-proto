e43ce73caea8bea7a3872a6970f9171a3fe1ae0e
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2026-04-01 22:32:13 -0400

    refactor io_ctrl (#904)
    * refactor io_ctrl
    
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
    
    * set flag
    
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

--- native/c/zam24.s
+++ native/c/zam24.s
@@ -8,41 +8,28 @@
 *  Copyright Contributors to the Zowe Project.
 ZAM24    RSECT ,
 ZAM24    AMODE 31
-ZAM24    RMODE 31                   Resolve 31-bit address
+ZAM24    RMODE 31
 *
          YREGS ,
 *
 *        On entry: R1=DCB abend parameter list, R14=return address
-*        Call ZAMDEXIT with valid C argument list, then return
+*        Call ZAMDEXIT with R1 preserved, then return
 *
 ENTRY24  DS    0H
-*        Save system return address and R1 in local storage
-         LARL  R15,MY_R14
-         ST    R14,0(,R15)
-         LARL  R15,MY_R1
-         ST    R1,0(,R15)
-*        Prepare R1 as a standard C parameter list
-         LR    R1,R15
-*        Branch to C code
-         LARL  R15,DCBABND@
+         USING IO_CTRL,R8
+*
+         ST    R1,WORK             Save PLIST in IO_CTRL "work" field
+         LARL  R15,ZAMDEXT@        ->-> ZAMDEXIT@
          L     R15,0(,R15)         -> ZAMDEXIT
          OILH  R15,X'8000'         Set AMODE 31 bit for BASSM
-         BASSM 14,R15              Call ZAMDEXIT in AMODE 31
-*        ZAMDEXIT returns here. R15 contains the return code.
-         LARL  R1,MY_R1
-         L     R1,0(,R1)           Restore original system R1
-         STC   R15,3(,R1)          Store RC into option_mask (byte 3)
-*        Restore R14 and return to system
-         LARL  R14,MY_R14
-         L     R14,0(,R14)
+         LA    R1,WORK              -> IO_CTRL "work" field
+         BASSM 0,R15               Call ZAMDEXIT in AMODE 31
          BR    R14                 Return to system
 *
 CONSTANT DS    0D
          LTORG ,
 *
-DCBABND@ DC    V(ZAMDEXIT)
-MY_R1    DS    A
-MY_R14   DS    A
+ZAMDEXT@ DC    V(ZAMDEXIT)
 *
 *        Separate ENTRY to obtain length of this module
 *
@@ -52,5 +39,14 @@ ZAM24Q   DS    0H
          BR    R14                  return to caller
 *
 ZQM24LEN EQU *-ZAM24                Dynamically obtain length of module
+*
+*        IO_CTRL structure (see zamtypes.h)
+*
+IO_CTRL  DSECT ,
+EYE      DS    CL8
+WORK     DS    D                    Work area
+         ORG   WORK                 Org back to work
+PLIST    DS    A                    PLIST address
+         ORG   ,                    Reset location counter
 *
          END   ,
