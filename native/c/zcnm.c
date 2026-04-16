ef8d405344b6e83d02decf0ae00422395621cd7f
Dan Kelosky <14940561+dkelosky@users.noreply.github.com>
2026-04-16 08:51:26 -0400

    add optimzation comment
    Signed-off-by: Dan Kelosky <14940561+dkelosky@users.noreply.github.com>

--- native/c/zcnm.c
+++ native/c/zcnm.c
@@ -19,6 +19,7 @@
 
 #pragma prolog(ZCNACT, " ZWEPROLG NEWDSA=(YES,128) ")
 #pragma epilog(ZCNACT, " ZWEEPILG ")
+// #pragma option_override(ZCNACT, "opt(level,0)")
 int ZCNACT(ZCN *zcn)
 {
   int rc = 0;
